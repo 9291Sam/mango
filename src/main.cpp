65e82dee742763b52460ce93f1c5d4373e6cde6f
9291Sam <sam.skinr@gmail.com>
2023-03-27 13:03:52 -0400

    working time strings

--- src/main.cpp
+++ src/main.cpp
@@ -21,18 +21,18 @@ const char* const levelAsString(Level l)
 {
     using enum Level;
     switch (l)
-        {
-        case Trace:
-            return "Trace";
-        case Debug:
-            return "Debug";
-        case Log:
-            return "Log";
-        case Warn:
-            return "Warn";
-        case Fatal:
-            return "Fatal";
-        }
+    {
+    case Trace:
+        return "Trace";
+    case Debug:
+        return "Debug";
+    case Log:
+        return "Log";
+    case Warn:
+        return "Warn";
+    case Fatal:
+        return "Fatal";
+    }
 }
 
 static constexpr std::array<std::string_view, 2> FOLDER_IDENTIFIERS {
@@ -46,38 +46,18 @@ void logFormatted(Level l, const std::source_location& loc, std::string msg)
         "[{0}] [{1} @ {2}:{3}:{4}] [{5}] {6}\n",
         [&]
         {
-            std::time_t cTime = std::time(nullptr);
+            std::string workingString {31, ' '};
 
-            // LIFETIME:
-            // this is the only place in the project where std::localtime is
-            // used as a result this is a pointer to a static
-            std::tm* localTime = std::localtime(&cTime);
+            workingString = fmt::format(
+                "{:0%b %m/%d/%Y %I:%M:}{:%S}",
+                fmt::localtime(std::time(nullptr)),
+                std::chrono::system_clock::now()
+            );
 
-            std::string bufferString {};
-            bufferString.resize(128);
+            workingString.at(workingString.size() - 7) = ':';
+            workingString.insert(workingString.size() - 3, ":");
 
-            if (std::strftime(
-                    bufferString.data(),
-                    bufferString.size(),
-                    "%b %m/%d/%Y %I:%M:%S %p",
-                    localTime
-                )
-                == 0)
-                {
-                    // panic! unable for
-                }
-
-            return bufferString;
-
-            // std::ti auto const time = std::chrono::current_zone()->to_local(
-            //     std::chrono::system_clock::now()
-            // );
-            // std::string timeString =
-            //     std::format("{:%b %m/%d/%Y %I:%M:%S %p}", time);
-
-            // return timeString.substr(0, 27).append(":").append(
-            //     timeString.substr(28)
-            // );
+            return workingString;
         }(), // 0
         loc.function_name(), // 1
         [&]
@@ -87,14 +67,14 @@ void logFormatted(Level l, const std::source_location& loc, std::string msg)
             std::size_t index = std::string::npos;
 
             for (std::string_view str : FOLDER_IDENTIFIERS)
+            {
+                if (index != std::string::npos)
                 {
-                    if (index != std::string::npos)
-                        {
-                            break;
-                        }
-
-                    index = raw_file_name.find(str);
+                    break;
                 }
+
+                index = raw_file_name.find(str);
+            }
 
             return raw_file_name.substr(index + 1);
         }(), // 2
@@ -146,7 +126,7 @@ MAKE_LOGGER(Fatal)
 
 int main()
 {
-    logLog("asdf");
+    logLog("{}", "asdf");
 
     std::string str = std::string {"invalid"};
 

6613e8279a83aa495d48f89a80a39907bf34667f
9291Sam <sam.skinr@gmail.com>
2023-04-18 19:36:46 -0400

    updated renderre

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 #include "gfx/renderer.hpp"
 #include "util/log.hpp"
-#include <chrono>
-#include <exception>
-#include <thread>
 
 int main()
 {
@@ -10,15 +7,9 @@ int main()
     {
         gfx::Renderer renderer {};
 
-        using namespace std::chrono_literals;
-
-        std::this_thread::sleep_for(1s);
-
         for (;;)
         {
             renderer.drawFrame();
-            util::logTrace("Frame finished drawing");
-            // util::panic("struck!");
         }
     }
     catch (const std::exception& e)
